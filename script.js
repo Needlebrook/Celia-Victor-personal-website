d748d946a3f83e09df1074c3d488c073d421ec90
Needlebrook <celiavictor101@gmail.com>
2025-06-21 00:34:06 +0530

    Initial commit on 21st june 2025

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    document.getElementById("spinner-overlay").style.display = "none";
+    document.getElementById("main-content").style.display = "block";
+  }, 1000);
+});
+
